cf0524e60976ea76f0022725298a98e4865c1b27
Jorge De la Vega <jorgedelavg04@gmail.com>
2020-06-09 13:50:11 -0500

    added update, needs fixes

--- src/app/reporte/service/reporte.service.ts
+++ src/app/reporte/service/reporte.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse} from '@angular/common/http';
+import { HttpClientModule, HttpErrorResponse, HttpHeaders, HttpClient } from '@angular/common/http';
+import { Report } from '../../all-reports/report';
 import { Observable, of, throwError } from 'rxjs';
 import { map, retry, catchError, tap } from 'rxjs/operators';
-import { Reporte } from '../models/reporte';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EstadoService {
+export class ReporteService {
 
-  reporte: Reporte[]
+  eporte: Report[];
+  
+  private messageSource = new BehaviorSubject<string>("default message");
+  currentMessage = this.messageSource.asObservable();
 
   getReportes(id: string): Observable<any> {
     console.log('estoy en el getALumnos');
@@ -25,25 +29,6 @@ export class EstadoService {
 
     return this.http.get('http://localhost:8080/api/reportes' + '/' + id, requestOptions).pipe(map(this.extractData), retry(3), catchError(this.handleError));
     //return this.http.get('http://localhost:8080/api/reportes', requestOptions);
-    //return this.http.get('http://localhost:8080/api/reportes', requestOptions);
-    //return this.http.get('http://localhost:8080/api/reportes' + '/' + '5edeeaf420f9a2324e58c8c3');
-    
-  }
-
-  deleteReportes(): Observable<any> {
-    console.log('estoy en el getALumnos');
-    var headerDict = {
-      'Content-Type': 'application/json',
-      Accept: '*/*',
-      'Access-Control-Allow-Origin': '*',
-    };
-
-    const requestOptions = {
-      headers: new HttpHeaders(headerDict),
-    };
-
-    //return this.http.delete('http://localhost:8080/api/reportes' + '/' + id, requestOptions).pipe(map(this.extractData), retry(3), catchError(this.handleError));
-    return this.http.get('http://localhost:8080/api/reportes', requestOptions);
     //return this.http.get('http://localhost:8080/api/reportes', requestOptions);
     //return this.http.get('http://localhost:8080/api/reportes' + '/' + '5edd22ddea83b761f8d358e5');
     
@@ -61,43 +46,40 @@ export class EstadoService {
       headers: new HttpHeaders(headerDict),
     };
 
-    return this.http.put('http://localhost:8080/api/reportes' + '/' + id, requestOptions);
+    //return this.http.put('http://localhost:8080/api/reportes' + '/' + id, requestOptions).pipe(map(this.extractData), retry(3), catchError(this.handleError));
+    return this.http.put('http://localhost:8080/api/reportes' + '/' + id, requestOptions)
     //return this.http.get('http://localhost:8080/api/reportes', requestOptions);
     //return this.http.get('http://localhost:8080/api/reportes', requestOptions);
     //return this.http.get('http://localhost:8080/api/reportes' + '/' + '5edd22ddea83b761f8d358e5');
-
-    
     
   }
 
 
-  getId(_id) {
-    return this.http.get('http://localhost:8080/api/reportes' + '/' + '5edd22ddea83b761f8d358e5');
-  }
-  
   private extractData(res: Response) {
-    let body = Array.of(res);
-    
+    let body = res;
+    console.log(body)
     return body|| {};
   }
-
+  
   handleError(error: HttpErrorResponse) {
     let errorMessage = 'Unknown error!';
     if (error.error instanceof ErrorEvent) {
       // Client-side errorsi
-      errorMessage = `Error: ${error.error.message}`;
+      errorMessage = `Error: ${error.error.message} al querer ingresar tus datos`;
     } else {
       // Server-side errors
-      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+      errorMessage = `Error: ${error.status}\nPor favor ingresa un número de reporte valido`;
     }
     window.alert(errorMessage);
     return throwError(errorMessage);
   }
+  
+  changeMessage(message: string) {
+    this.messageSource.next(message)
+    console.log(message)
+  }
 
-
-  //getReports() {
-   // return REPORTS;
-  //}
+  
 
   constructor(private http: HttpClient) { }
 }
