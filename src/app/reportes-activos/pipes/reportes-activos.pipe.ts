16094989155dd5c3fef8f4256c21f15cdc3b3c8d
Jorge De la Vega <jorgedelavg04@gmail.com>
2020-06-12 10:46:44 -0500

    ya funciona yei

---
+++ src/app/reportes-activos/pipes/reportes-activos.pipe.ts
@@ -0,0 +1,49 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class ReportesActivosPipe implements PipeTransform {
+  report = '';
+  
+  transform(values: any[], filterPost: any): any[] {
+    if(!values) return [];
+    if(!filterPost) return values;
+
+    filterPost = filterPost.toLowerCase();
+    if (filterPost.indexOf != undefined) {
+      return (values.filter(report => report.nombre.toLowerCase().includes(filterPost)));
+    }  
+        
+      /*
+      Por _id
+      transform(values: any[], filterPost: any): any[] {
+      if(!values) return [];
+      if(!filterPost) return values;
+
+      filterPost = filterPost.toLowerCase();
+      console.log(filterPost)
+
+      if (filterPost.indexOf != undefined) {
+        return (values.filter(report => report._id.includes(filterPost)));
+      }
+      
+      Por nombre:
+      transform(report: Report[], filterPost: string) {
+        if(!report || !filterPost){
+          return report;
+        }else {
+          console.log("report",report)
+          console.log("filterpost",filterPost)
+          return (report.filter(report => report.nombre == filterPost))
+        } 
+      
+    }
+
+
+      */
+      
+  }
+
+}
+
